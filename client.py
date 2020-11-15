ffaab11903a62e33743ccae828b3d09460a02eac
Dmytro Soboliev <sobolev.dmirtiy@gmail.com>
2020-11-15 23:34:17 +0100

    Working process

--- client.py
+++ client.py
@@ -5,3 +5,35 @@ class Client:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.connect(settings.SERVER_ADDRESS)
+
+    def start(self):
+        print("Client started")
+        self.execute_work()
+
+    def send_message(self, **kwargs):
+        json_data = json.dumps(kwargs)
+        self.client.send(json_data.encode("utf-8"))
+        return f"Message to server sent successfully."
+
+    def get_full_message(self):
+        response = self.client.recv(settings.BUFFER_SIZE)
+        print(response)
+
+        response = json.loads(response.decode("utf-8"))
+        return response
+
+    def execute_work(self):
+        print("Starting the work!")
+        while True:  # бесконечно
+            message = self.get_full_message()
+
+            if message.get('action'):
+                action = message['action']
+
+                if action == 'data':
+                    self.data.append(message['data'])
+                elif action == "operation":
+                        result = eval(
+                        message['data'].join(str(x) for x in self.data))
+                        self.send_message(data=result, action='ok')
+                        self.data = []
--- server.py
+++ server.py
@@ -13,32 +13,55 @@ class Server:
         print("Server Started")
         self.accept_clients
 
-    def check_ready(self):  # проверяем, если у нас достаточно клиентов для выполнения операций
-        return len(self.clients) >= 2  # проверяем, у нас больше 2 клиентов или нет
+    def check_ready(self):
+        return len(self.clients) >= 2
     def execute_work(self):
-        print('Genereting random matrix')
+        print("Work!")
         new_array = [[random.randint(0, 10) for x in range(3)] for y in range(3)]
-        new_array2 = [[random.randint(0, 10) for x in range(3)] for y in range(3)]
-        new_array3 = [[random.randint(0, 10) for x in range(3)] for y in range(3)]
-        print(f"Code generated 3 random arrays.")
         print(new_array)
+        new_array2 = [[random.randint(0, 10) for x in range(3)] for y in range(3)]
         print(new_array2)
+        new_array3 = [[random.randint(0, 10) for x in range(3)] for y in range(3)]
         print(new_array3)
+        print(f"Code generated 3 random arrays.")
+
+        try:
+            print(self.send_message(socket='b', data=new_array, action='data'))
+
+            print(self.send_message(socket='b', data=new_array2, action='data'))
+            print(self.send_message(socket='c', data=new_array3, action='data'))
+
+            print(self.send_message(socket='b', data='+', action='operation'))
+
+
+        except KeyError as exc:
+            print(exc, file=stderr)
+            self.disconnect()
+            return
+
+        response = self.get_full_message(self.clients['b'])
+        if response.get('action') == 'error':
+            print(f"Error, {response['data']}", file=stderr)
+            return
+
+        print(f"B returned '{response}' response")
+        print(self.send_message(socket='c', data=response['data'], action='data'))
+        print(self.send_message(socket='c', data='+', action='operation'))
+
+
+        response = self.get_full_message(self.clients['c'])
+
+        if response.get('action') == 'error':
+            print(f"Error, {response['data']}", file=stderr)
+            return
+        print(f"Everything is executed. The result is: {response.get('data', 'No result')}. Stopping the job")
+        self.disconnect()
+
 
 
     def accept_clients(self):
         while True:
             client_socket, client_address = self.server.accept()
             print(f"Client from address '{client_address}' connected.")
-
-            try:
-                self.__client_id += 1
-                self.clients[chr(self.__client_id)] = client_socket
-            except ValueError:
-                print("Maximum client number reached! Cannot connect more!")
-                return
-
-            if self.check_ready():
-                self.execute_work()
-            else:
-                print("Not enough clients to execute the processes. Waiting for more to connect.")
+            self.__client_id += 1
+            self.clients[chr(self.__client_id)] = client_socket
