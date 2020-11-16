9eb9d60803a24ca50f4264e0733f97943b982f93
Dmytro Soboliev <sobolev.dmirtiy@gmail.com>
2020-11-16 09:22:26 +0100

    Working commit

--- client.py
+++ client.py
@@ -5,6 +5,7 @@ class Client:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.connect(settings.SERVER_ADDRESS)
+        self.data = []
 
     def start(self):
         print("Client started")
@@ -16,16 +17,30 @@ class Client:
         return f"Message to server sent successfully."
 
     def get_full_message(self):
-        response = self.client.recv(settings.BUFFER_SIZE)
-        print(response)
+        try:
+            response = self.client.recv(settings.BUFFER_SIZE)
+        except ConnectionError:
+            print("Connection aborted")
+            self.client.close()
+            return
 
-        response = json.loads(response.decode("utf-8"))
-        return response
+        print(response)
+        if not response:
+            return
+
+        try:
+            response = json.loads(response.decode("utf-8"))
+            return response
+        except UnicodeDecodeError and json.JSONDecodeError:
+            print("Cannot encode the response. Stopping the job")
+            self.send_message(data='Cannot decode the response', action='error')
 
     def execute_work(self):
         print("Starting the work!")
-        while True:  # бесконечно
+        while True:
             message = self.get_full_message()
+            if not message:
+                return
 
             if message.get('action'):
                 action = message['action']
@@ -33,7 +48,18 @@ class Client:
                 if action == 'data':
                     self.data.append(message['data'])
                 elif action == "operation":
-                        result = eval(
-                        message['data'].join(str(x) for x in self.data))
+                    try:
+                        result = eval(message['data'].join(str(x) for x in self.data))
                         self.send_message(data=result, action='ok')
                         self.data = []
+                    except Exception as exc:
+                        print(exc, file=stderr)
+                        self.send_message(data=str(exc), action='error')
+
+            else:
+                print("No action provided!", file=stderr)
+
+
+if __name__ == '__main__':
+    server = Client()
+    server.execute_work()
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import socket, random, settings
+import socket, random, json, settings
 from sys import stderr
 
 class Server:
@@ -15,6 +15,36 @@ class Server:
 
     def check_ready(self):
         return len(self.clients) >= 2
+
+    def send_message(self, **kwargs):
+        try:
+            where = kwargs['socket']
+            del kwargs['socket']
+            json_data = json.dumps(kwargs)
+            self.clients[where].send(json_data.encode("utf-8"))
+        except KeyError as exc:
+            raise exc
+        else:
+            return f"Message to {where} sent successfully."
+
+    def get_full_message(self, clients_socket):
+        print(f"Getting the message from {clients_socket}")
+        try:
+            response = clients_socket.recv(settings.BUFFER_SIZE)
+        except ConnectionError as exc:
+            print(exc, file=stderr)
+            return
+
+        try:
+            response = json.loads(response.decode("utf-8"))
+            return response
+        except UnicodeDecodeError or json.JSONDecodeError:
+            print("Cannot encode the response. Stopping the job", file=stderr)
+
+        def disconnect(self):
+            for client in self.clients.values():
+                client.close()
+
     def execute_work(self):
         print("Work!")
         new_array = [[random.randint(0, 10) for x in range(3)] for y in range(3)]
@@ -27,12 +57,10 @@ class Server:
 
         try:
             print(self.send_message(socket='b', data=new_array, action='data'))
-
             print(self.send_message(socket='b', data=new_array2, action='data'))
             print(self.send_message(socket='c', data=new_array3, action='data'))
 
             print(self.send_message(socket='b', data='+', action='operation'))
-
 
         except KeyError as exc:
             print(exc, file=stderr)
@@ -40,28 +68,48 @@ class Server:
             return
 
         response = self.get_full_message(self.clients['b'])
+        if not response:
+            return
+
         if response.get('action') == 'error':
             print(f"Error, {response['data']}", file=stderr)
             return
 
         print(f"B returned '{response}' response")
-        print(self.send_message(socket='c', data=response['data'], action='data'))
-        print(self.send_message(socket='c', data='+', action='operation'))
-
+        try:
+            print(self.send_message(socket='c', data=response['data'], action='data'))
+            print(self.send_message(socket='c', data='+', action='operation'))
+        except KeyError as exc:
+            print(exc, file=stderr)
 
         response = self.get_full_message(self.clients['c'])
+        if not response:
+            return
 
         if response.get('action') == 'error':
             print(f"Error, {response['data']}", file=stderr)
             return
-        print(f"Everything is executed. The result is: {response.get('data', 'No result')}. Stopping the job")
+        print(f"Everything is executed. The result is: {response.get('data', 'No result')}. Stopping the job") # выводим результат
         self.disconnect()
-
-
 
     def accept_clients(self):
         while True:
             client_socket, client_address = self.server.accept()
             print(f"Client from address '{client_address}' connected.")
-            self.__client_id += 1
-            self.clients[chr(self.__client_id)] = client_socket
+
+            try:
+                self.__client_id += 1
+                self.clients[chr(self.__client_id)] = client_socket
+                # будем обращаться по буквам
+            except ValueError:  # если chr() выходит за рамки
+                print("Maximum client number reached! Cannot connect more!")
+                return
+
+            if self.check_ready():
+                self.execute_work()
+            else:
+                print("Not enough clients to execute the processes. Waiting for more to connect.")
+
+if __name__ == '__main__':
+    server = Server()
+    server.start()
