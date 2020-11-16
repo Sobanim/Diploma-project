a160342d56ceb4fcf482271e531e772fc44d30cf
Dmytro Soboliev <sobolev.dmirtiy@gmail.com>
2020-11-16 09:39:34 +0100

    Add comments

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ class Server:
         self.server.listen()
 
         self.clients = {}
-        self.__client_id = 97 # id client, 97 == 'a'
+        self.__client_id = 97 # id client, 97 == 'a', 98 == 'b'
     def start(self):
         print("Server Started")
         self.accept_clients
@@ -16,13 +16,13 @@ class Server:
     def check_ready(self):
         return len(self.clients) >= 2
 
-    def send_message(self, **kwargs):
+    def send_message(self, **kwargs): # kwargs - array with variable with names
         try:
-            where = kwargs['socket']
-            del kwargs['socket']
+            where = kwargs['socket'] # where send data
+            del kwargs['socket'] # delete clients dont know
             json_data = json.dumps(kwargs)
             self.clients[where].send(json_data.encode("utf-8"))
-        except KeyError as exc:
+        except KeyError as exc: # if we dont have kwargs[socket], but we have address to send
             raise exc
         else:
             return f"Message to {where} sent successfully."
@@ -41,9 +41,9 @@ class Server:
         except UnicodeDecodeError or json.JSONDecodeError:
             print("Cannot encode the response. Stopping the job", file=stderr)
 
-        def disconnect(self):
-            for client in self.clients.values():
-                client.close()
+    def disconnect(self):
+        for client in self.clients.values():
+            client.close()
 
     def execute_work(self):
         print("Work!")
@@ -56,18 +56,18 @@ class Server:
         print(f"Code generated 3 random arrays.")
 
         try:
-            print(self.send_message(socket='b', data=new_array, action='data'))
+            print(self.send_message(socket='b', data=new_array, action='data')) # send array to client b
             print(self.send_message(socket='b', data=new_array2, action='data'))
             print(self.send_message(socket='c', data=new_array3, action='data'))
 
-            print(self.send_message(socket='b', data='+', action='operation'))
+            print(self.send_message(socket='b', data='+', action='operation')) # send operation to client b
 
         except KeyError as exc:
             print(exc, file=stderr)
             self.disconnect()
             return
 
-        response = self.get_full_message(self.clients['b'])
+        response = self.get_full_message(self.clients['b']) # get responce from b
         if not response:
             return
 
@@ -77,19 +77,19 @@ class Server:
 
         print(f"B returned '{response}' response")
         try:
-            print(self.send_message(socket='c', data=response['data'], action='data'))
-            print(self.send_message(socket='c', data='+', action='operation'))
+            print(self.send_message(socket='c', data=response['data'], action='data')) # send responce to C
+            print(self.send_message(socket='c', data='+', action='operation')) # send operation to C
         except KeyError as exc:
             print(exc, file=stderr)
 
-        response = self.get_full_message(self.clients['c'])
+        response = self.get_full_message(self.clients['c']) # final result
         if not response:
             return
 
         if response.get('action') == 'error':
             print(f"Error, {response['data']}", file=stderr)
             return
-        print(f"Everything is executed. The result is: {response.get('data', 'No result')}. Stopping the job") # выводим результат
+        print(f"Everything is executed. The result is: {response.get('data', 'No result')}. Stopping the job") # print result
         self.disconnect()
 
     def accept_clients(self):
@@ -98,10 +98,10 @@ class Server:
             print(f"Client from address '{client_address}' connected.")
 
             try:
-                self.__client_id += 1
+                self.__client_id += 1 # unique letter for each client
                 self.clients[chr(self.__client_id)] = client_socket
-                # будем обращаться по буквам
-            except ValueError:  # если chr() выходит за рамки
+
+            except ValueError:
                 print("Maximum client number reached! Cannot connect more!")
                 return
 
